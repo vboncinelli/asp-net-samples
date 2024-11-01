dfd76b746423fd7bda6c10f233f8968832c74d77
Vanni Boncinelli <vanni.boncinelli@giuneco.it>
2024-11-01 10:21:31 +0100

    update

--- text-adventure-mvc/text-adventure.mvc/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace text_adventure.mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
